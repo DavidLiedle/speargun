fd3cab20255e54a2f68491284868be652ed95049
David Liedle <david.liedle@gmail.com>
2015-11-21 08:55:05 -0700

    API Key File
    Checks for the existence of the API key before proceeding.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # speargun
 An API client for working with DigitalOcean in Dart
+
+# Status: Learning
+I'm using this project as a fun combo-exercise both to become familiar with DigitalOcean's API and to try building a finished CLI App in Dart.
+
+# Installation
+Create a file called `DigitalOceanAPI.key` in the same directory as the `speargun.dart` script. Make sure you've got the DartVM installed on your system! I'm working with 1.13.0 as I write this on a MacBook Air with El Capitan 10.11.1 and Homebrew's Dart installed.
+
+# Usage
+You can use `dart speargun.dart` to run with Dart, or just run `speargun`, a BASH shell script to run it as a single command.
---
+++ speargun.dart
@@ -0,0 +1,33 @@
+/**
+ * speargun - a command-line interface to DigitalOcean's API in Dart
+ *
+ * As I iterate through the learning process to build this out, I'm starting here:
+ * https://www.dartlang.org/docs/tutorials/cmdline/
+ *
+ */
+
+//import 'dart:async';
+//import 'dart:convert';
+import 'dart:io';
+
+//import 'package:args/args.dart';
+
+const LINE_NUMBER = 'line-number';
+var NEWLINE = '\n';
+
+//ArgResults argResults;
+
+void main(List<String> arguments){
+	print('speargun initializing...');
+
+  // Check for the API key file: https://api.dartlang.org/134830/dart-io/File-class.html
+  if( new File('DigitalOceanAPI.key').existsSync() ){ // https://api.dartlang.org/134830/dart-io/FileSystemEntity/existsSync.html
+    print('Got the key!');
+  } else {
+    print('OHNOES! You need to create a DigitalOceanAPI.key file with your DigitalOcean API key pasted as plain text inside...');
+  }
+
+	// final parser = new ArgParser()
+ //    ..addFlag(LINE_NUMBER, negatable: false, abbr: 'n');
+
+}
